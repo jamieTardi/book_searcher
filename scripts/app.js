a676a3bb034f0943970570a4a63e7c206150270e
jamieTardi <jamie.tardi@gmail.com>
2020-10-08 16:44:41 +0100

    api works

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,7 @@ const apiCode = 'AIzaSyDEnA5musfa3p1DBS_ePjyrCXFqX5TUZms'
 const searchInput = document.querySelector('.search-input')
 const submitBtn = document.querySelector('.search-button')
 
-let searchRtn = searchInput.value
+let searchRtn = ''
 
 
 async function fetchApi(url) {
@@ -18,14 +18,19 @@ async function fetchApi(url) {
 }
 
 async function stuff () {
-    let fetchLink = `https://www.googleapis.com/books/v1/volumes?q=cheese+inauthor:keyes&key=${apiCode}`
+    let fetchLink = `https://www.googleapis.com/books/v1/volumes?q=${searchRtn}+intitle:${searchRtn}&key=${apiCode}`
     const data = await fetchApi(
         fetchLink  
       );
       console.log(data)
-      console.log(data.items[0].searchInfo)
+      console.log(data.items[0].volumeInfo.authors[0])
 }
 
-console.log(searchRtn)
-stuff()
 
+
+
+submitBtn.addEventListener('click', () => {
+   searchRtn = searchInput.value
+   console.log(searchRtn)
+   stuff()
+})
