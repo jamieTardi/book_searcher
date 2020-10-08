e6d1bdf9ae751ec66007700e63b25939a55c16ca
jamieTardi <jamie.tardi@gmail.com>
2020-10-08 17:29:46 +0100

    finished inital api

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,17 @@
 const apiCode = 'AIzaSyDEnA5musfa3p1DBS_ePjyrCXFqX5TUZms'
 const searchInput = document.querySelector('.search-input')
 const submitBtn = document.querySelector('.search-button')
+const author = document.querySelector('.author-name')
+const bookType = document.querySelector('.type-name')
+const bookPic = document.querySelector('.book-pic')
+const bookDesc = document.querySelector('.book-name')
 
 let searchRtn = ''
 
 
+
 async function fetchApi(url) {
-    const dataFetch = await fetch(url,{
+    const dataFetch = await fetch(url, {
         method: 'GET',
         headers: {
             Accept: 'application/json',
@@ -17,20 +22,30 @@ async function fetchApi(url) {
     return data;
 }
 
-async function stuff () {
+async function stuff() {
     let fetchLink = `https://www.googleapis.com/books/v1/volumes?q=${searchRtn}+intitle:${searchRtn}&key=${apiCode}`
     const data = await fetchApi(
-        fetchLink  
-      );
-      console.log(data)
-      console.log(data.items[0].volumeInfo.authors[0])
+        fetchLink
+    );
+    let newBackground = data.items[0].volumeInfo.imageLinks.thumbnail
+    console.log(data)
+    console.log(data.items[0].volumeInfo)
+    author.innerHTML = data.items[0].volumeInfo.authors[0]
+    bookType.innerHTML = data.items[0].volumeInfo.categories[0]
+    bookPic.style.background = `url('${newBackground}')`;
+    bookPic.style.backgroundRepeat = 'none'
+    bookPic.style.backgroundSize = 'cover'
+    bookDesc.innerHTML = data.items[0].volumeInfo.title
+
+
+
 }
 
 
 
 
 submitBtn.addEventListener('click', () => {
-   searchRtn = searchInput.value
-   console.log(searchRtn)
-   stuff()
+    searchRtn = searchInput.value
+    console.log(searchRtn)
+    stuff()
 })
